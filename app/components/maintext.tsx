5ffb5caa66c91e5a5d58e1421cd4d598585d652a
Amarsanaa Oyunbat <amarsanaa@Amarsanaas-MacBook-Pro.local>
2023-11-17 18:33:44 -0600

    second commit

---
+++ app/components/maintext.tsx
@@ -0,0 +1,29 @@
+import { ReactNode } from "react";
+export const MainText = () => {
+    return <div style={{paddingLeft:"139px"}}>
+
+        <h1 style={{
+            color:"white",
+            width: "514px",
+            height: "120px",
+            fontFamily: "Work Sans",
+            fontWeight: "400",
+            fontSize: "48px",
+            lineHeight: "56,3px",
+         
+        }}>Instant collaborations for remote teams</h1>
+        <p style={{
+            color:"white",
+            width:"323.85px",
+            height:"58px",
+            fontFamily:"Mulish",
+            fontWeight:"700",
+           fontSize:"18px",
+           lineHeight:"29.37px",
+           top:"252px",
+           left:"124px",
+           paddingTop:"22px",
+         
+        }}>All in one for your remote team chats, collaboration and track projects</p>
+    </div>
+};
---
+++ app/components/navigation-item.tsx
@@ -0,0 +1,20 @@
+import { ReactNode } from "react";
+export const NavigationItem = ({
+    children,
+    href,
+}: {
+    children: ReactNode;
+    href?: string;
+}) => {
+    return (
+        <a
+            href={href || "/"}
+            style={{
+                color: "white",
+                textDecorationColor: "white",
+                lineHeight:"28.11px",            }}
+        >
+            {children}
+        </a>
+    );
+};
